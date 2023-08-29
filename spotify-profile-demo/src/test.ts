e0a8c3652e7adf9ee51fb28e39087dd225c88180
Will Propst <propstwt@appstate.edu>
2023-08-29 00:57:37 -0400

    Working on trying to set up top tracks page

--- spotify-profile-demo/src/test.ts
+++ spotify-profile-demo/src/test.ts
@@ -1,4 +1,9 @@
 import { SpotifyApi } from "@spotify/web-api-ts-sdk";
+import type {
+  UserResponse,
+  Page,
+  Track,
+} from "../node_modules/@spotify/web-api-ts-sdk/src/types";
 
 const clientId = "7666b9483fa648d097ec418979d47c55";
 const scopes = [
@@ -11,10 +16,49 @@ const scopes = [
 
 const sdk = SpotifyApi.withUserAuthorization(
   clientId,
-  "http://localhost:5173",
+  "http://localhost:5173/callback",
   scopes
 );
 
-const profile = sdk.currentUser.profile();
+let page_id = document.body.id;
 
-const country = profile.country;
+switch (page_id) {
+  case "profile_view":
+    const profile: UserResponse =
+      (await sdk.currentUser.profile()) as UserResponse;
+    populateProfileUI(profile);
+
+    break;
+  case "tracks":
+    const track: Page<Track> = (await sdk.currentUser.topItems(
+      "tracks",
+      "long_term",
+      10
+    )) as unknown as Page<Track>;
+    console.log(track.items[0]);
+    document.getElementById("1")!.innerText = track.items[0].name + " ";
+    break;
+  default:
+    break;
+}
+
+// const profile: UserResponse = (await sdk.currentUser.profile()) as UserResponse;
+
+function populateProfileUI(profile: UserResponse) {
+  document.getElementById("displayName")!.innerText = profile.display_name;
+  if (profile.images[0]) {
+    const profileimage = new Image(100, 100);
+    profileimage.src = profile.images[0].url;
+    document.getElementById("avatar")!.appendChild(profileimage);
+  }
+  document.getElementById("id")!.innerText = profile.id;
+  document.getElementById("email")!.innerText = profile.email;
+  document.getElementById("uri")!.innerText = profile.uri;
+  document
+    .getElementById("uri")!
+    .setAttribute("href", profile.external_urls.spotify);
+  document.getElementById("url")!.innerText = profile.href;
+  document.getElementById("url")!.setAttribute("href", profile.href);
+  document.getElementById("imgUrl")!.innerText =
+    profile.images[0]?.url ?? "(no profile image)";
+}
