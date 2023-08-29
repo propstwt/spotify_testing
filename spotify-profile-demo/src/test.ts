2bf764a7647890957fbf70e095c874aa3346b693
Will Propst <propstwt@appstate.edu>
2023-08-28 21:37:28 -0400

    Fixed package setup

--- spotify-profile-demo/src/test.ts
+++ spotify-profile-demo/src/test.ts
@@ -1,13 +1,20 @@
 import { SpotifyApi } from "@spotify/web-api-ts-sdk";
 
-
 const clientId = "7666b9483fa648d097ec418979d47c55";
-const scopes = ["user-read-private", "user-read-email", "user-read-currently-playing",
-                "user-top-read", "user-library-read"];
+const scopes = [
+  "user-read-private",
+  "user-read-email",
+  "user-read-currently-playing",
+  "user-top-read",
+  "user-library-read",
+];
 
+const sdk = SpotifyApi.withUserAuthorization(
+  clientId,
+  "http://localhost:5173",
+  scopes
+);
 
-const sdk = SpotifyApi.withUserAuthorization(clientId, "http://localhost:5173", scopes);
-
-const profile = sdk.currentUser.profile()
+const profile = sdk.currentUser.profile();
 
 const country = profile.country;
